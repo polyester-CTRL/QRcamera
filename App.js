502a40f8ca7422197ecf53f1bda7b3d5166fda8d
polyester_ctrl <shunfreed2016@gmail.com>
2021-02-23 02:37:44 +0900

    バーコード読み取り機能を実装

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { Camera } from 'expo-camera'; 
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [scanned, setScanned] = useState(false);
+  
+  if (Platform.OS === 'web') {
+    if (!Camera.isAvailableAsync()) {
+      
+      return <Text>No access to camera</Text>
+    }
+    // setType(Camera.Constants.Type.front);
+  } else {
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
+  
+  
+    if (hasPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+  }
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Camera
+        style={styles.camera}
+        type={type}
+        // barCodeScannerSettings={{
+        //   shouldRenderIndicator: true,
+        //   barCodeTypes: ['qr',],
+        // }}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      >
+        <View style={styles.buttonContainer}>
+          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +70,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    // marginHorizontal: 15,
+    // marginBottom: 10,
+  },
+  noCamera: {
+    color: '#888',
+    fontSize: 20,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
